1556d7e67aba1d9afc75c1799e57f36678b5b1fd
Sarthak Sain <Sarthaksain08@gmail.com>
2025-09-06 18:23:14 +0530

    W

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,30 +1,29 @@
-import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from '@/components/ui/button';
-import { dummyInterviews } from "@/constants";
+
+import { Button } from "@/components/ui/button";
 import InterviewCard from "@/components/InterviewCard";
-import { get } from 'http';
-import { getCurrentUser, getLatestInterviews } from '@/lib/actions/auth.action';
-import { getInterviewsByUserId } from '@/lib/actions/general.action';
 
+import { getCurrentUser } from "@/lib/actions/auth.action";
+import {
+  getInterviewsByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
 
-const  Page  = async () => {
+async function Home() {
   const user = await getCurrentUser();
 
-  const [userInterviews , latestInterviews] =  await Promise.all([
-    await getInterviewsByUserId(user?.id!),
-    await getLatestInterviews({userId: user?.id!})
+  const [userInterviews, allInterview] = await Promise.all([
+    getInterviewsByUserId(user?.id!),
+    getLatestInterviews({ userId: user?.id! }),
   ]);
 
-
-  const hasPastInterviews = userInterviews?.length > 0;
-  const hasUpcomingInterviews = latestInterviews?.length > 0;
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = allInterview?.length! > 0;
 
   return (
-    <section>
-      
-      <section className='card-cta'>
+    <>
+      <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
           <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
           <p className="text-lg">
@@ -45,41 +44,51 @@ const  Page  = async () => {
         />
       </section>
 
-      {/* Your Interviews Section */}
-      <section className='flex flex-col gap-6 mt-8'> 
+      <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
 
-        <div className='interviews-section'>
-          {
-            hasPastInterviews ? (
-              userInterviews?.map((interview) => (
-                <InterviewCard  {...interview} key={interview.id} />
-              ))) : (
-              <p>ou&apos;t taken any interviews yet</p>
-            )}
+        <div className="interviews-section">
+          {hasPastInterviews ? (
+            userInterviews?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
 
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take Interviews</h2>
 
-      {/* Take an Interview Section */}
-      <section className='flex flex-col gap-6 mt-8'> 
-        <h2>Take an Interview</h2>
-        <div className='interviews-section'>
-
-                    {
-            hasUpcomingInterviews ? (
-              latestInterviews?.map((interview) => (
-                <InterviewCard  {...interview} key={interview.id} />
-              ))) : (
-              <p>There are no new interviews available  </p>
-            )}
-
-
+        <div className="interviews-section">
+          {hasUpcomingInterviews ? (
+            allInterview?.map((interview) => (
+              <InterviewCard
+                key={interview.id}
+                userId={user?.id}
+                interviewId={interview.id}
+                role={interview.role}
+                type={interview.type}
+                techstack={interview.techstack}
+                createdAt={interview.createdAt}
+              />
+            ))
+          ) : (
+            <p>There are no interviews available</p>
+          )}
         </div>
       </section>
-    </section>
-  )
+    </>
+  );
 }
 
-export default Page
-                          
+export default Home;
