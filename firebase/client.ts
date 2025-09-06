ed62bbb1aa44b74435460d26d9aafe75a8ef2047
Sarthak Sain <Sarthaksain08@gmail.com>
2025-09-06 18:42:43 +0530

    hi

--- firebase/client.ts
+++ firebase/client.ts
@@ -10,13 +10,20 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDYCVeOvwqAkCpLSP5DI1NzKbfA-FWve0Q",
-  authDomain: "mockmate-47199.firebaseapp.com",
-  projectId: "mockmate-47199",
-  storageBucket: "mockmate-47199.firebasestorage.app",
-  messagingSenderId: "266250476131",
-  appId: "1:266250476131:web:f215c203e2e88350d5f656",
-  measurementId: "G-QFM11ZBFBL"
+  // apiKey: "AIzaSyDYCVeOvwqAkCpLSP5DI1NzKbfA-FWve0Q",
+  // authDomain: "mockmate-47199.firebaseapp.com",
+  // projectId: "mockmate-47199",
+  // storageBucket: "mockmate-47199.firebasestorage.app",
+  // messagingSenderId: "266250476131",
+  // appId: "1:266250476131:web:f215c203e2e88350d5f656",
+  // measurementId: "G-QFM11ZBFBL"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
